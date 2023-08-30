68b5fe3c27ce9bd02929dca2e84e8137e4ff9e4e
Michael Mikowski <mmikowski@kfocus.org>
2023-08-29 21:55:20 -0700

    3613 Revise files for kfocus-firstrun-bin name change

--- package-firstrun-bin/main.cpp
+++ package-firstrun-bin/main.cpp
@@ -75,13 +75,13 @@ int main(int argc, char *argv[])
     cryptDisks.removeLast();
     dat.setCryptDiskList(cryptDisks);
 
-    // Check for the presence of a second kfocus-welcome-bin instance
+    // Check for the presence of a second kfocus-firsturn-bin instance
     ShellEngine duplicateFinder;
-    // NOTE: We only search for "kfocus-welcome" and not "kfocus-welcome-bin"
-    // here because for some unknown reason kfocus-welcome-bin shows up as
-    // "kfocus-welcome-" (yes, with a weird dash at the end) in the output of
+    // NOTE: We only search for "kfocus-firstrun" and not "kfocus-firstrun-bin"
+    // here because for some unknown reason kfocus-firstrun-bin shows up as
+    // "kfocus-firsturn-" (yes, with a weird dash at the end) in the output of
     // "ps axo comm". Why this is, I have no clue.
-    duplicateFinder.execSync("ps axo comm | grep kfocus-welcome");
+    duplicateFinder.execSync("ps axo comm | grep kfocus-firstrun");
     QStringList outputLines = duplicateFinder.stdout().split('\n');
     if (outputLines.length() > 2) { // there's always one blank line
         // TODO 2023-08-24 arraybolt3 notice: Replace kdialog here?
