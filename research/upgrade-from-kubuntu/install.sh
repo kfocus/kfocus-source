736892122d2d37d30f55822d1092e7453baf9f28
Aaron Rainbolt <arraybolt3@gmail.com>
2023-11-11 17:24:53 -0600

    3288 tidy up code some

--- research/upgrade-from-kubuntu/install.sh
+++ research/upgrade-from-kubuntu/install.sh
@@ -1,73 +1,127 @@
 #!/bin/bash
-_rx_dir='/var/lib/kfocus';
-mkdir -p "${_rx_dir}" || exit;
-echo '1.3.0-0' > "${_rx_dir}/focusrx_version";
+#
+# Copyright 2023 MindShare Inc.
+#
+# Written for the Kubuntu Focus by M. Mikowski and A. Rainbolt.
+#
+# Name      : install.sh
+# Summary   : install.sh -[i|c]
+# Purpose   : Backend for kfocus-suite-manager that handles Kubuntu Focus Suite
+#             installation related tasks.
+# Example   : install.sh -i
+# Arguments : -i: Install the suite. Must be run as root.
+#             -c: Configure the current user account to use the suite. Must
+#                 be run as the user being configured (NOT AS ROOT!).
+# License   : GPLv2
+# Run By    : kfocus-suite-manager
+#
+set -u;
 
-if ! apt-get -y -f install; then
-  exit 1;
-fi
+## BEGIN _installSuiteFn {
+_installSuiteFn () {
+  declare _rx_dir _install_success _install_attempts;
 
-if ! dpkg --configure -a; then
-  exit 1;
-fi
+  _rx_dir='/var/lib/kfocus';
+  mkdir -p "${_rx_dir}" || exit;
+  echo '1.3.0-0' > "${_rx_dir}/focusrx_version";
 
-apt list --installed | awk -F '/' '{print $1}' > "${_rx_dir}/pre-install-pkgs.list";
-apt-cache policy |sed '/^Pinned packages:/q' | grep -iE '^\s*release' > "${_rx_dir}/pre-install-repos.list";
-
-dpkg --add-architecture i386;
-add-apt-repository -y multiverse;
-
-if ! add-apt-repository -y ppa:kfocus-team/release; then
-  exit 1;
-fi
-
-# Retry if this fails due to https timeouts from launchpad or others.
-_install_success='no';
-_install_attempts=0;
-while [ "${_install_success}" = 'no' ] && [ "${_install_attempts}" != '3' ]; do
-  if ! DEBIAN_FRONTEND=noninteractive \
-    apt-get -y install kfocus-apt-source; then
-    _install_attempts=$((_install_attempts + 1));
-    continue;
+  if ! apt-get -y -f install; then
+    exit 1;
   fi
 
-  apt-get update;
-
-  if ! DEBIAN_FRONTEND=noninteractive apt-get -y install kfocus-main; then
-    _install_attempts=$((_install_attempts + 1));
-    continue;
+  if ! dpkg --configure -a; then
+    exit 1;
   fi
 
-  if ! DEBIAN_FRONTEND=noninteractive \
-    apt-get -y purge google-chrome-unstable; then
-    _install_attempts=$((_install_attempts + 1));
-    continue;
+  apt list --installed | awk -F '/' '{print $1}' > "${_rx_dir}/pre-install-pkgs.list";
+  apt-cache policy |sed '/^Pinned packages:/q' | grep -iE '^\s*release' > "${_rx_dir}/pre-install-repos.list";
+
+  dpkg --add-architecture i386;
+  add-apt-repository -y multiverse;
+
+  if ! add-apt-repository -y ppa:kfocus-team/release; then
+    exit 1;
   fi
 
-  apt-get update;
+  # Retry if this fails due to https timeouts from launchpad or others.
+  _install_success='no';
+  _install_attempts='0';
+  while [ "${_install_success}" = 'no' ] && [ "${_install_attempts}" != '3' ]; do
+    if ! DEBIAN_FRONTEND=noninteractive \
+      apt-get -y install kfocus-apt-source; then
+      _install_attempts=$((_install_attempts + 1));
+      continue;
+    fi
 
-  if ! DEBIAN_FRONTEND=noninteractive apt-get -y full-upgrade; then
-    _install_attempts=$((_install_attempts + 1));
-    continue;
+    apt-get update;
+
+    if ! DEBIAN_FRONTEND=noninteractive apt-get -y install kfocus-main; then
+      _install_attempts=$((_install_attempts + 1));
+      continue;
+    fi
+
+    if ! DEBIAN_FRONTEND=noninteractive \
+      apt-get -y purge google-chrome-unstable; then
+      _install_attempts=$((_install_attempts + 1));
+      continue;
+    fi
+
+    apt-get update;
+
+    if ! DEBIAN_FRONTEND=noninteractive apt-get -y full-upgrade; then
+      _install_attempts=$((_install_attempts + 1));
+      continue;
+    fi
+
+    if ! DEBIAN_FRONTEND=noninteractive \
+      apt-get -y install linux-generic-hwe-22.04-kfocus \
+      linux-headers-generic-hwe-22.04-kfocus \
+      linux-image-generic-hwe-22.04-kfocus \
+      linux-tools-generic-hwe-22.04-kfocus; then
+      _install_attempts=$((_install_attempts + 1));
+      continue;
+    fi
+
+    if ! DEBIAN_FRONTEND=noninteractive apt-get -y autopurge; then
+      _install_attempts=$((_install_attempts + 1));
+      continue;
+    fi
+
+    _install_success='yes';
+  done
+
+  if [ "${_install_success}" = 'no' ]; then
+    exit 1;
   fi
+}
+## . END _installSuiteFn }
 
-  if ! DEBIAN_FRONTEND=noninteractive \
-    apt-get -y install linux-generic-hwe-22.04-kfocus \
-    linux-headers-generic-hwe-22.04-kfocus \
-    linux-image-generic-hwe-22.04-kfocus \
-    linux-tools-generic-hwe-22.04-kfocus; then
-    _install_attempts=$((_install_attempts + 1));
-    continue;
-  fi
+## BEGIN _configureSuiteFn {
+_configureSuiteFn () {
+  # TODO: fill me in
+}
+## . END _configureSuiteFn }
 
-  if ! DEBIAN_FRONTEND=noninteractive apt-get -y autopurge; then
-    _install_attempts=$((_install_attempts + 1));
-    continue;
-  fi
+## BEGIN _mainFn {
+_mainFn () {
+  declare _arg_str;
 
-  _install_success='yes';
-done
+  _arg_str="${1:-}";
+  case "${_arg_str}" in
+    '-i') _installSuiteFn;;
+    '-c') _configureSuiteFn;;
+      '') _cm2ErrStrFn 'Flag required'; exit 1;;
+  esac
+}
+## . END _mainFn }
 
-if [ "${_install_success}" = 'no' ]; then
-  exit 1;
+declare _binName _binDir _baseDir _baseName;
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  _binName="$(  readlink -f "$0"       )" || exit 101;
+  _binDir="$(   dirname  "${_binName}" )" || exit 101;
+  _baseDir="$(  dirname  "${_binDir}"  )" || exit 101;
+  _baseName="$( basename "${_binName}" )" || exit 101;
+
+  _mainFn "$@";
 fi
--- research/upgrade-from-kubuntu/uninstall.sh
+++ research/upgrade-from-kubuntu/uninstall.sh
@@ -1,78 +1,208 @@
 #!/bin/bash
-_rx_dir='/var/lib/kfocus';
-_repo_dir='/etc/apt/sources.list.d';
+#
+# Copyright 2023 MindShare Inc.
+#
+# Written for the Kubuntu Focus by M. Mikowski and A. Rainbolt.
+#
+# Name      : uninstall.sh
+# Summary   : uninstall.sh
+# Purpose   : Backend for kfocus-suite-manager that handles Kubuntu Focus Suite
+#             uninstallation related tasks.
+# Example   : uninstall.sh
+# Arguments : None, however the script does start a two-way communication with
+#             the script that executed it. It will provide the name of a repo
+#             and then read a string from stdin. If the string is 'y' it will
+#             purge the repo it provided the name of, if the string is 'n' it
+#             will preserve it.
+# License   : GPLv2
+# Run By    : kfocus-suite-manager
+#
+set -u
+
 source lib/kfocus-packagetree.source
 
-_uninstall_success='no';
-_uninstall_attempts=0;
-  
-_foundPkgList=( 'kfocus-main' );
-_preInstallPkgStr="$(cat "${_rx_dir}/pre-install-pkgs.list";)" || exit 1;
-_loadKfPkgListFn;
-_pre_install_repo_str="$(cat "${_rx_dir}/pre-install-repos.list")";
+## BEGIN _handleBitRepoFn {
+_handleBitRepoFn () {
+  declare _arg_str;
 
-while [ "${_uninstall_success}" = 'no' ] && [ "${_uninstall_attempts}" != '3' ]; do
+  _arg_str="${1:-}";
+
+  if [ "${_arg_str}" = 'y' ]; then
+    if ! DEBIAN_FRONTEND=noninteractive \
+      ppa-purge -y -o 'bit-team' -p 'stable'; then
+      echo 'FAIL';
+      exit 1;
+    fi
+  fi
+}
+## . END _handleBitRepoFn }
+
+## BEGIN _handleKubuntuBackportsRepoFn {
+_handleKubuntuBackportsExtraRepoFn () {
+  declare _arg_str;
+
+  _arg_str="${1:-}";
+
+  if [ "${_arg_str}" = 'y' ]; then
+    if ! DEBIAN_FRONTEND=noninteractive \
+      ppa-purge -y -o 'kubuntu-ppa' -p 'backports-extra'; then
+      echo 'FAIL';
+      exit 1;
+    fi
+  fi
+}
+## . END _handleKubuntuBackportsRepoFn }
+
+## BEGIN _handleKubuntuBackportsRepoFn {
+_handleKubuntuBackportsRepoFn () {
+  declare _arg_str;
+
+  _arg_str="${1:-}";
+
+  if [ "${_arg_str}" = 'y' ]; then
+    if ! DEBIAN_FRONTEND=noninteractive \
+      ppa-purge -y -o 'kubuntu-ppa' -p 'backports'; then
+      echo 'FAIL';
+      exit 1;
+    fi
+  fi
+}
+## . END _handleKubuntuBackportsRepoFn }
+
+## BEGIN _handleKeepassRepoFn {
+_handleKeepassRepoFn () {
+  declare _arg_str;
+
+  _arg_str="${1:-}";
+
+  if [ "${_arg_str}" = 'y' ]; then
+    if ! DEBIAN_FRONTEND=noninteractive \
+      ppa-purge -y -o 'phoerious' -p 'keepassxc'; then
+      echo 'FAIL';
+      exit 1;
+    fi
+  fi
+}
+## . END _handleKeepassRepoFn
+
+## BEGIN _handleUbuntuStudioBackportsRepoFn {
+_handleUbuntuStudioBackportsRepoFn () {
+  declare _arg_str;
+
+  _arg_str="${1:-}";
+
+  if [ "${_arg_str}" = 'y' ]; then
+    if ! DEBIAN_FRONTEND=noninteractive \
+      ppa-purge -y -o 'ubuntustudio-ppa' -p 'backports'; then
+      echo 'FAIL';
+      exit 1;
+    fi
+  fi
+}
+## . END _handleUbuntuStudioBackportsRepoFn }
+
+## BEGIN _handleNvidiaCudaRepoFn {
+_handleNvidiaCudaRepoFn () {
+  declare _arg_str;
+
+  _arg_str="${1:-}";
+
+  if [ "${_arg_str}" = 'y' ]; then
+    if ! DEBIAN_FRONTEND=noninteractive \
+      ppa-purge -s 'developer.download.nvidia.com' -o 'compute' -p 'cuda'; then
+      echo 'FAIL';
+      exit 1;
+    fi
+  fi
+}
+## . END _handleNvidiaCudaRepoFn }
+
+## BEGIN _handleGoogleChromeRepoFn {
+_handleGoogleChromeRepoFn () {
+  declare _arg_str;
+
+  _arg_str="${1:-}";
+
+  if [ "${_arg_str}" = 'y' ]; then
+    if ! DEBIAN_FRONTEND=noninteractive \
+      ppa-purge -s 'dl.google.com' -o 'linux' -p 'chrome'; then
+      echo 'FAIL';
+      exit 1;
+    fi
+  fi
+}
+## . END _handleGoogleChromeRepoFn }
+
+## BEGIN _mainFn {
+_mainFn () {
+  declare _rx_dir _repo_dir _pre_install_repo_str _purge_str _input_str;
+
+  # _foundPkgList and _preInstallPkgStr provided by kfocus-packagetree.source
+
+  _rx_dir='/var/lib/kfocus';
+  _repo_dir='/etc/apt/sources.list.d';
+
+  _foundPkgList=( 'kfocus-main' );
+  _preInstallPkgStr="$(cat "${_rx_dir}/pre-install-pkgs.list";)" || exit 1;
+  _loadKfPkgListFn;
+
+  _pre_install_repo_str="$(cat "${_rx_dir}/pre-install-repos.list")";
+
   if ! apt-get -y install linux-generic-hwe-22.04 \
     linux-headers-generic-hwe-22.04 \
     linux-image-generic-hwe-22.04 \
     linux-tools-generic-hwe-22.04; then
-    _uninstall_attempts=$((_uninstall_attempts + 1));
-    continue;
+    echo 'FAIL';
+    exit 1;
   fi
   
-  if ! DEBIAN_FRONTEND=noninteractive ppa-purge -y -o kfocus-team -p release; then
-    _uninstall_attempts=$((_uninstall_attempts + 1));
-    continue;
+  if ! DEBIAN_FRONTEND=noninteractive \
+    ppa-purge -y -o kfocus-team -p release; then
+    echo 'FAIL';
+    exit 1;
   fi
 
   if ! [[ "${_pre_install_repo_str}" =~ 'o=LP-PPA-bit-team-stable' ]]; then
-    if ! DEBIAN_FRONTEND=noninteractive ppa-purge -y -o bit-team -p stable; then
-      _uninstall_attempts=$((_uninstall_attempts + 1));
-      continue;
-    fi
+    echo 'BackInTime';
+    read _input_str;
+    _handleBitRepoFn "${_input_str}";
+  fi
+  
+  if ! [[ "${_pre_install_repo_str}" =~ 'o=LP-PPA-kubuntu-ppa-backports-extra' ]]; then
+    echo 'kubuntu-backports-extra';
+    read _input_str;
+    _handleKubuntuBackportsExtraRepoFn "${_input_str}";
   fi
 
   if ! [[ "${_pre_install_repo_str}" =~ 'o=LP-PPA-kubuntu-ppa-backports' ]]; then
-    if ! DEBIAN_FRONTEND=noninteractive ppa-purge -y -o kubuntu-ppa -p backports; then
-      _uninstall_attempts=$((_uninstall_attempts + 1));
-      continue;
-    fi
-  fi
-
-  if ! [[ "${_pre_install_repo_str}" =~ 'o=LP-PPA-kubuntu-ppa-backports-extra' ]]; then
-    if ! DEBIAN_FRONTEND=noninteractive ppa-purge -y -o kubuntu-ppa -p backports-extra; then
-      _uninstall_attempts=$((_uninstall_attempts + 1));
-      continue;
-    fi
+    echo 'kubuntu-backports';
+    read _input_str;
+    _handleKubuntuBackportsRepoFn "${_input_str}";
   fi
   
   if ! [[ "${_pre_install_repo_str}" =~ 'o=LP-PPA-phoerious-keepassxc' ]]; then
-    if ! DEBIAN_FRONTEND=noninteractive ppa-purge -y -o phoerious -p keepassxc; then
-      _uninstall_attempts=$((_uninstall_attempts + 1));
-      continue;
-    fi
+    echo 'keepassxc';
+    read _input_str;
+    _handleKeepassRepoFn "${_input_str}";
   fi
 
   if ! [[ "${_pre_install_repo_str}" =~ 'o=LP-PPA-ubuntustudio-ppa-backports' ]]; then
-    if ! DEBIAN_FRONTEND=noninteractive ppa-purge -y -o ubuntustudio-ppa -p backports; then
-      _uninstall_attempts=$((_uninstall_attempts + 1));
-      continue;
-    fi
+    echo 'ubuntustudio-backports';
+    read _input_str;
+    _handleUbuntuStudioBackportsRepoFn "${_input_str}";
   fi
 
   if ! [[ "${_pre_install_repo_str}" =~ 'o=NVIDIA,l=NVIDIA CUDA' ]]; then
-    if ! DEBIAN_FRONTEND=noninteractive ppa-purge -s developer.download.nvidia.com -o compute -p cuda; then
-      _uninstall_attempts=$((_uninstall_attempts + 1));
-      continue;
-    fi
+    echo 'nvidia-cuda';
+    read _input_str;
+    _handleNvidiaCudaRepoFn "${_input_str}";
   fi
 
   # TODO: Perhaps tighten up this check
   if ! [[ "${_pre_install_repo_str}" =~ 'o=Google LLC' ]]; then
-    if ! DEBIAN_FRONTEND=noninteractive ppa-purge -s dl.google.com -o linux -p chrome; then
-      _uninstall_attempts=$((_uninstall_attempts + 1));
-      continue;
-    fi
+    echo 'google-chrome';
+    read _input_str;
+    _handleGoogleChromeRepoFn "${_input_str}";
   fi
 
   # TODO: These repos are REALLY HARD to remove with ppa-purge
@@ -96,8 +226,20 @@ while [ "${_uninstall_success}" = 'no' ] && [ "${_uninstall_attempts}" != '3' ];
   fi
 
   _uninstall_success="yes";
-done
 
-if [ "${_uninstall_success}" = "no" ]; then
-  exit 1;
+  if [ "${_uninstall_success}" = "no" ]; then
+    exit 1;
+  fi
+}
+## . END _mainFn }
+
+declare _binName _binDir _baseDir _baseName;
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  _binName="$(  readlink -f "$0"       )" || exit 101;
+  _binDir="$(   dirname  "${_binName}" )" || exit 101;
+  _baseDir="$(  dirname  "${_binDir}"  )" || exit 101;
+  _baseName="$( basename "${_binName}" )" || exit 101;
+
+  _mainFn;
 fi
