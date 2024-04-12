8543133995688dea3655267099171e578e8bdfa1
Aaron Rainbolt <arraybolt3@gmail.com>
2024-04-11 20:03:59 -0500

    Bug fixes in package-calamares-settings

--- package-calamares-settings/kfocus/oem/calamares-oemfinish.sh
+++ package-calamares-settings/kfocus/oem/calamares-oemfinish.sh
@@ -2,6 +2,6 @@
 #
 # Remove OEM files from the system.
 
-rm -rf /etc/calamares /usr/bin/basicwallpaper /usr/bin/calamares-finish-oem /usr/share/applications/calamares-finish-oem.desktop /usr/share/xsessions/kubuntu-oem-environment.desktop /usr/libexec/start-kubuntu-oem-env /etc/sudoers /usr/bin/calamares-logs-helper /usr/libexec/calamares-oemfinish.sh
+rm -rf /etc/calamares /usr/bin/basicwallpaper /usr/bin/calamares-finish-oem /usr/share/applications/calamares-finish-oem.desktop /usr/share/xsessions/kubuntu-oem-environment.desktop /usr/libexec/start-kubuntu-oem-env /etc/sudoers /usr/bin/calamares-logs-helper /usr/libexec/calamares-oemfinish.sh /usr/libexec/kfocus-prep-user
 
 mv /etc/sudoers.orig /etc/sudoers
