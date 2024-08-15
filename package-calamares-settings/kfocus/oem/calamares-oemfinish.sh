0ff8481d146cc8208d683499607d651295988996
Aaron Rainbolt <arraybolt3@ubuntu.com>
2024-08-15 00:58:01 -0500

    4574 Move kfocus-chain-helper to kfocus-calamares-settings, flesh it out more

--- package-calamares-settings/kfocus/oem/calamares-oemfinish.sh
+++ package-calamares-settings/kfocus/oem/calamares-oemfinish.sh
@@ -2,6 +2,6 @@
 #
 # Remove OEM files from the system.
 
-rm -rf /etc/calamares /usr/bin/basicwallpaper /usr/bin/calamares-finish-oem /usr/share/applications/calamares-finish-oem.desktop /usr/share/xsessions/kubuntu-oem-environment.desktop /usr/libexec/start-kubuntu-oem-env /etc/sudoers /usr/bin/calamares-logs-helper /usr/libexec/calamares-oemfinish.sh /usr/libexec/kfocus-prep-user
+rm -rf /etc/calamares /usr/bin/basicwallpaper /usr/bin/calamares-finish-oem /usr/share/applications/calamares-finish-oem.desktop /usr/share/xsessions/kubuntu-oem-environment.desktop /usr/libexec/start-kubuntu-oem-env /etc/sudoers /usr/bin/calamares-logs-helper /usr/libexec/calamares-oemfinish.sh /usr/libexec/kfocus-prep-user /usr/lib/kfocus/bin/kfocus-chain-helper-* /usr/share/applications/kfocus-chain-helper.desktop
 
 mv /etc/sudoers.orig /etc/sudoers
