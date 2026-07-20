10ff0663f521fb6b501bc467e4bdf21ed0e187d6
Michael Mikowski <mmikowski@kfocus.org>
2026-07-20 15:37:04 -0400

    6567 Refminements for kbd support; works zrg2 and m2g7

--- kfocus-kb-disable.sh
+++ kfocus-kb-disable.sh
@@ -1,20 +1,20 @@
 # This branch has all controls disabled.
 # Do these steps to apply to your running system.
-if sudo systemctl disable --now kfocus-kb-save; then
-  echo 'kfocus-kb-save disabled.';
+if sudo systemctl enable --now kfocus-kb-save; then
+  echo 'kfocus-kb-save enabled.';
 else
-  echo 'kfocus-kb-save NOT disabled.';
+  echo 'kfocus-kb-save NOT enabled.';
   echo 'PLEASE FIX!';
 fi
 
-meld \
+sudo meld \
   package-hw/usr/lib/kfocus/conf/usr_lib_systemd_system-sleep_zsleep-kfocus \
   /usr/lib/systemd/system-sleep/zsleep-kfocus;
 
-while read _file; do
-  _basename="$(basename "${_file}")";
-  sudo meld "${_file}" \
-    "/usr/lib/kfocus/bin/${_basename}";
+while read _path; do
+  sudo meld "${_path}" "/${_path#*/}";
 done < <(
-  git diff --name-only RR-2026-Q3 | grep kfocus/bin
+  git diff --name-only RR-2026-Q3 | grep kfocus/bin;
+  echo package-tools/usr/lib/kfocus/bin/kfocus-kb-color-set;
+  echo package-settings/usr/share/kfocus/kf5-settings;
 );
