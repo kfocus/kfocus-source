2ac8378256964f90e60057b858e45a51cd790e61
Michael Mikowski <mmikowski@kfocus.org>
2026-07-20 12:29:04 -0400

    6567 Update test script to disable sleep-restore service

--- kfocus-kb-disable.sh
+++ kfocus-kb-disable.sh
@@ -1,6 +1,16 @@
 # This branch has all controls disabled.
 # Do these steps to apply to your running system.
-sudo systemctl disable --now kfocus-kb-save
+if sudo systemctl disable --now kfocus-kb-save; then
+  echo 'kfocus-kb-save disabled.';
+else
+  echo 'kfocus-kb-save NOT disabled.';
+  echo 'PLEASE FIX!';
+fi
+
+sudo meld \
+  package-hw/usr/lib/kfocus/conf/usr_lib_systemd_system-sleep_zsleep-kfocus \
+  /usr/lib/systemd/system-sleep/zsleep-kfocus;
+
 git diff --name-only RR-2026-Q3 \
   |grep kfocus/bin \
   | while read _file; do
