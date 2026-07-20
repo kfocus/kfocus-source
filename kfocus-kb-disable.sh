e813d4bc8764aabdbe50bd9ca9496946f4dce4d1
Aaron Rainbolt <arraybolt3@gmail.com>
2026-07-20 11:13:54 -0500

    6567 ^kfocus-kb-disable.sh use sudo on meld, ^kfocus-power-set fix element count check

--- kfocus-kb-disable.sh
+++ kfocus-kb-disable.sh
@@ -5,6 +5,6 @@ git diff --name-only RR-2026-Q3 \
   |grep kfocus/bin \
   | while read _file; do
   _basename="$(basename "${_file}")";
-  meld "${_file}" \
+  sudo meld "${_file}" \
     "/usr/lib/kfocus/bin/${_basename}";
 done
