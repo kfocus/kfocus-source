615a3e2b3d3f870a159997799efd9ec22d244834
Michael Mikowski <mmikowski@kfocus.org>
2026-07-20 12:10:02 -0400

    6567 More removal of kfocus-kb-color-set usage

--- kfocus-kb-disable.sh
+++ kfocus-kb-disable.sh
@@ -1,11 +1,10 @@
 # This branch has all controls disabled.
 # Do these steps to apply to your running system.
 sudo systemctl disable --now kfocus-kb-save
-cd kfocus-source
 git diff --name-only RR-2026-Q3 \
   |grep kfocus/bin \
   | while read _file; do
   _basename="$(basename "${_file}")";
-  sudo meld "${_file}" \
+  meld "${_file}" \
     "/usr/lib/kfocus/bin/${_basename}";
 done
