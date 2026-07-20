838fd2da79411877608806bbdb7d87a15488f239
Michael Mikowski <mmikowski@kfocus.org>
2026-07-20 13:51:37 -0400

    6567 ^powermanagmentprofilerc=>powerdevilrc; +zr kbd color resume restore

--- kfocus-kb-disable.sh
+++ kfocus-kb-disable.sh
@@ -7,14 +7,14 @@ else
   echo 'PLEASE FIX!';
 fi
 
-sudo meld \
+meld \
   package-hw/usr/lib/kfocus/conf/usr_lib_systemd_system-sleep_zsleep-kfocus \
   /usr/lib/systemd/system-sleep/zsleep-kfocus;
 
-git diff --name-only RR-2026-Q3 \
-  |grep kfocus/bin \
-  | while read _file; do
+while read _file; do
   _basename="$(basename "${_file}")";
   sudo meld "${_file}" \
     "/usr/lib/kfocus/bin/${_basename}";
-done
+done < <(
+  git diff --name-only RR-2026-Q3 | grep kfocus/bin
+);
