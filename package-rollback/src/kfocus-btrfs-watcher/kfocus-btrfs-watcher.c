0323ec9e6fed96b71457429c4fcfb774774c0e61
Aaron Rainbolt <arraybolt3@gmail.com>
2026-09-10 16:04:14 -0500

    6865 ^package-rollback move maintenance trigger into kfocus-btrfs-watcher

--- package-rollback/src/kfocus-btrfs-watcher/kfocus-btrfs-watcher.c
+++ package-rollback/src/kfocus-btrfs-watcher/kfocus-btrfs-watcher.c
@@ -30,8 +30,18 @@
 #include <poll.h>
 #include <stdbool.h>
 #include <limits.h>
+#include <signal.h>
+#include <assert.h>
 
 #define HOUR_SEC_COUNT 3600
+#define MAINT_TIMER_PATH "/var/lib/kfocus/btrfs_maint_timer"
+/* 4 hours, in milliseconds */
+#define MAINT_TIMER_EXP_MS 14400000
+/* 5 minutes, in milliseconds */
+#define MAINT_BOOT_THRESH_MS 300000
+
+bool main_loop_active = false;
+bool prepare_to_terminate = false;
 
 void *safe_calloc(size_t nmemb, size_t size) {
   void *ptr = calloc(nmemb, size);
@@ -46,6 +56,13 @@ enum fs_info {
   SIZE,
   USED
 };
+
+void signal_handler(int signal_num) {
+  if (!main_loop_active) {
+    _exit(0);
+  }
+  prepare_to_terminate = true;
+}
 
 uint64_t get_btrfs_fs_info(int fd, const char *path, enum fs_info info_type) {
   struct btrfs_ioctl_fs_info_args fi_args = { 0 };
@@ -90,7 +107,7 @@ bool timespec_compare_ge(struct timespec tsbig, struct timespec tssmall) {
 
 int get_poll_timeout(struct timespec *debounce_ts_list,
   struct timespec *debounce_max_ts_list, bool *fs_mod_flag_list,
-  size_t path_data_len, struct timespec ts) {
+  size_t path_data_len, uint32_t maint_timer_ms, struct timespec ts) {
 
   int solve_timeout_ms = INT_MAX;
   int current_timeout;
@@ -121,11 +138,131 @@ int get_poll_timeout(struct timespec *debounce_ts_list,
     }
   }
 
-  if (solve_timeout_ms == INT_MAX) {
-    return -1;
+  if (maint_timer_ms > INT_MAX) {
+    maint_timer_ms = INT_MAX;
   }
+  current_timeout = MAINT_TIMER_EXP_MS - (int)(maint_timer_ms);
+  if (current_timeout < 0) {
+    current_timeout = 0;
+  }
+  if (current_timeout < solve_timeout_ms) {
+    solve_timeout_ms = current_timeout;
+  }
+
+  assert(solve_timeout_ms < INT_MAX);
+  assert(solve_timeout_ms > 0);
   solve_timeout_ms += 1; /* don't trigger just before a timer expires */
   return solve_timeout_ms;
+}
+
+uint32_t load_maint_timer() {
+  int maint_timer_fd = 0;
+  off_t maint_timer_len = 0;
+  char *maint_timer_buf = NULL;
+  ssize_t maint_read_len = 0;
+  uint8_t read_attempt_count = 0;
+  uint32_t ret_val = 0;
+  char *check_ptr = NULL;
+  unsigned long parse_rslt = 0;
+
+  maint_timer_fd = open(MAINT_TIMER_PATH, O_RDONLY);
+  if (maint_timer_fd == -1) {
+    goto cleanup;
+  }
+  maint_timer_len = lseek(maint_timer_fd, 0, SEEK_END);
+  if (maint_timer_len <= 0) {
+    goto cleanup;
+  }
+  /* Largest possible 32-bit integer is 10 decimal digits, take into account
+   * the newline at the end of text files */
+  if (maint_timer_len > 11) {
+    goto cleanup;
+  }
+  maint_timer_buf = safe_calloc((size_t)(maint_timer_len), sizeof(char));
+
+  /* Retrying the read until we get a complete read is easy. This is only 11
+   * characters max, so the chances of us getting a partial read are
+   * vanishingly small. Give up after three tries. */
+  while (maint_read_len != maint_timer_len && read_attempt_count < 3) {
+    if (lseek(maint_timer_fd, 0, SEEK_SET) == -1) {
+      goto cleanup;
+    }
+    maint_read_len = read(maint_timer_fd, maint_timer_buf,
+      (size_t)(maint_timer_len));
+    read_attempt_count++;
+  }
+  close(maint_timer_fd);
+  maint_timer_fd = 0;
+  if (maint_read_len != maint_timer_len) {
+    goto cleanup;
+  }
+
+  /* Validate and NULL-terminate */
+  if (maint_timer_buf[0] == '\0') {
+    goto cleanup;
+  }
+  if (maint_timer_buf[maint_timer_len - 1] != '\n') {
+    goto cleanup;
+  }
+  maint_timer_buf[maint_timer_len - 1] = '\0';
+
+  /* Technically if there is a NULL earlier in the string, this could be
+   * fooled, but the file is owned by root, so we don't have to be worried
+   * about it being malicious. */
+  parse_rslt = strtoul(maint_timer_buf, &check_ptr, 10);
+  if (*check_ptr != '\0') {
+    goto cleanup;
+  }
+  if (parse_rslt > UINT32_MAX) {
+    goto cleanup;
+  }
+  ret_val = (uint32_t)(parse_rslt);
+
+cleanup:
+  if (maint_timer_fd > 0) {
+    close(maint_timer_fd);
+  }
+  if (maint_timer_buf != NULL) {
+    free(maint_timer_buf);
+  }
+  return ret_val;
+}
+
+void try_save_maint_timer(uint32_t maint_timer_ms) {
+  int maint_timer_fd = 0;
+  char *timer_buf = NULL;
+  int timer_buf_len = 0;
+  ssize_t write_len = 0;
+
+  maint_timer_fd = open(MAINT_TIMER_PATH, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+  if (maint_timer_fd == -1) {
+    goto cleanup;
+  }
+
+  timer_buf_len = snprintf(NULL, 0, "%d\n", maint_timer_ms);
+  if (timer_buf_len < 0) {
+    goto cleanup;
+  }
+  timer_buf_len++;
+  timer_buf = safe_calloc((size_t)(timer_buf_len), sizeof(char));
+  snprintf(timer_buf, (size_t)(timer_buf_len), "%d\n", maint_timer_ms);
+
+  write_len = write(maint_timer_fd, timer_buf, (size_t)(timer_buf_len - 1));
+  if (write_len == -1) {
+    perror("Could not save timestamp");
+    exit(1);
+  }
+  if (write_len != timer_buf_len - 1) {
+    fprintf(stderr, "WARNING: Partial write while saving timestamp!\n");
+  }
+
+cleanup:
+  if (maint_timer_fd > 0) {
+    close(maint_timer_fd);
+  }
+  if (timer_buf != NULL) {
+    free(timer_buf);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -167,6 +304,26 @@ int main(int argc, char **argv) {
   ssize_t fanlen;
   struct fanotify_event_metadata *fem = NULL;
   struct timespec ts = { 0 };
+  struct timespec last_ts = { 0 };
+  uint32_t maint_timer_ms = 0;
+  uint32_t elapsed_ms = 0;
+  struct sigaction act = { 0 };
+
+  /* Set up a signal handler */
+  act.sa_handler = signal_handler;
+  sigemptyset(&act.sa_mask);
+  if (sigaction(SIGTERM, &act, NULL) == -1) {
+    perror("Cannot set up signal handler");
+    exit(1);
+  }
+
+  /* Initialize timestamp */
+  if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
+    fprintf(stderr, "failed to get time: ");
+    perror(NULL);
+    exit(1);
+  }
+  last_ts = ts;
 
   /* Allocate memory for working variable arrays */
   while (true) {
@@ -220,7 +377,8 @@ int main(int argc, char **argv) {
     }
     fan_fd_list[i] = fanotify_init(FAN_CLASS_NOTIF | FAN_CLOEXEC, O_CLOEXEC);
     if (fan_fd_list[i] < 0) {
-      fprintf(stderr, "Failed to initialize fanotify watcher for |%s|: ", current_path);
+      fprintf(stderr, "Failed to initialize fanotify watcher for |%s|: ",
+        current_path);
       perror(NULL);
       exit(1);
     }
@@ -246,7 +404,18 @@ int main(int argc, char **argv) {
     fs_alloc_threshold_list[i] = (fs_size_list[i] * threshold_pct_list[i]) / 100;
   }
 
+  /* Load maintenance timer duration from file */
+  maint_timer_ms = load_maint_timer();
+  /* Avoid triggering maintenance within five minutes of bootup
+   * Need to check both before and after adding MAINT_BOOT_THRESH_MS to
+   * account for potential overflow */
+  if (maint_timer_ms >= MAINT_TIMER_EXP_MS
+    || (maint_timer_ms + MAINT_BOOT_THRESH_MS) >= MAINT_TIMER_EXP_MS) {
+    maint_timer_ms = MAINT_TIMER_EXP_MS - MAINT_BOOT_THRESH_MS;
+  }
+
   /* fanotify event loop */
+  main_loop_active = true;
   while (
     poll(
       fan_poll_list,
@@ -256,15 +425,22 @@ int main(int argc, char **argv) {
         debounce_max_ts_list,
         fs_mod_flag_list,
         path_data_len,
+        maint_timer_ms,
         ts
       )
-    ) != -1) {
+    ) != -1 || errno == EINTR) {
 
     if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
       fprintf(stderr, "failed to get time: ");
       perror(NULL);
       exit(1);
     }
+    elapsed_ms = (uint32_t)(
+        ((ts.tv_sec      * 1000) + (ts.tv_nsec      / 1000000))
+      - ((last_ts.tv_sec * 1000) + (last_ts.tv_nsec / 1000000))
+    );
+    maint_timer_ms += elapsed_ms;
+    last_ts = ts;
 
     for (size_t i = 0; i < path_data_len; ++i) {
       if (fan_poll_list[i].revents & POLLIN) {
@@ -341,6 +517,18 @@ int main(int argc, char **argv) {
           exit(1);
         }
       }
+    }
+
+    if (maint_timer_ms >= MAINT_TIMER_EXP_MS) {
+      if (system("/usr/lib/kfocus/bin/kfocus-focusrx-system -s") != 0) {
+        fprintf(stderr, "WARNING: |kfocus-focusrx-system -s| errored out!\n");
+      }
+      maint_timer_ms = 0;
+    }
+
+    if (prepare_to_terminate) {
+      try_save_maint_timer(maint_timer_ms);
+      exit(0);
     }
   }
 
